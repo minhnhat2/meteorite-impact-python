5eb5b8fca6217f20b004e075977997714f7b04d8
Lê Minh Nhật <111627866+minhnhat2002@users.noreply.github.com>
2023-01-14 15:26:53 +0700

    Update meteorite impact.py

--- meteorite impact.py
+++ meteorite impact.py
@@ -8,23 +8,22 @@ pygame.init()
 #tạo màn hình
 screen= pygame.display.set_mode((width,height))
 #tạo nhân vật người chơi
-player=pygame.Rect(width/2,height/2,20,20) #tạo một đối tượng người chơi có chiều rộng là 20 và chiều cao là 20, đồng thời đặt đối tượng đó ở giữa màn hình.
-#tiểu hành tinh
+player=pygame.Rect(width/2,height/2,20,20) 
 asteroids=[]
 for i in range (10):
-    x=random.randint(0,width)        ###tạo ra 10 đối tượng tiểu hành tinh, mỗi đối tượng có vị trí x và y ngẫu nhiên trong màn hình, chiều rộng và chiều cao là 50.
+    x=random.randint(0,width)     
     y=random.randint(0,height)
     asteroids.append(pygame.Rect(x,y,50,50))
 #nét chữ
 font=pygame.font.Font(None,30)
 #clock
-clock=pygame.time.Clock() #tạo đối tượng đồng hồ để kiểm soát tốc độ khung hình của trò chơi.
+clock=pygame.time.Clock()
 #score
 score=0
 # vòng lặp trò chơi
-while True:  #vòng lặp trò chơi chính chạy cho đến khi người dùng đóng trò chơi.
+while True:  
     #xử lý sự kiện
-    for event in pygame.event.get():  #xử lý mọi sự kiện, chẳng hạn như người dùng đóng trò chơi.
+    for event in pygame.event.get(): 
         if event.type==pygame.QUIT:
             pygame.quit()
             sys.exit()
@@ -39,7 +38,7 @@ while True:  #vòng lặp trò chơi chính chạy cho đến khi người dùng
     if keys[pygame.K_DOWN] or keys[pygame.K_s]:
         player.y+=5
     #cập nhật vị trí người chơi
-    player.clamp_ip(pygame.Rect(0,0,width,height)) #đảm bảo rằng trình phát nằm trong ranh giới màn hình.
+    player.clamp_ip(pygame.Rect(0,0,width,height)) 
     #cập nhật vị trí của từng tiểu hành tinh và nếu một tiểu hành tinh biến mất khỏi màn hình, 
     #nó sẽ được định vị lại ở đầu màn hình với vị trí x ngẫu nhiên.
     for asteroid in asteroids:
@@ -47,23 +46,21 @@ while True:  #vòng lặp trò chơi chính chạy cho đến khi người dùng
         if asteroid.y>height:
             asteroid.y=0
             asteroid.x=random.randint(0,width)
-        #kiểm tra va chạm
-        #kiểm tra va chạm giữa người chơi và từng tiểu hành tinh và nếu va chạm xảy ra, 
-        #điểm sẽ tăng lên và tiểu hành tinh được định vị lại ở đầu màn hình với vị trí x ngẫu nhiên.
+ 
         for asteroid in asteroids:
             if player.colliderect(asteroid):
                 score+=1
                 asteroid.x=random.randint(0,width)
                 asteroid.y=0
     #vẽ đồ vật
-    screen.fill((0,0,0)) #lấp đầy màn hình bằng một màu đen.
-    for asteroid in asteroids: #vẽ từng tiểu hành tinh trên màn hình.
+    screen.fill((0,0,0)) 
+    for asteroid in asteroids: ]
         pygame.draw.rect(screen,(255,255,255),asteroid)
-    pygame.draw.rect(screen, (255,0,0), player) #vẽ người chơi trên màn hình.
+    pygame.draw.rect(screen, (255,0,0), player) ]
     #vẽ score
-    text=font.render(f"Score: {score}",True,(255,255,255)) #tạo một đối tượng văn bản hiển thị điểm hiện tại bằng màu trắng.
-    screen.blit(text, (10,10)) #vẽ đối tượng văn bản trên màn hình tại vị trí (10,10).
+    text=font.render(f"Score: {score}",True,(255,255,255)) 
+    screen.blit(text, (10,10))
     #cập nhật màn hình
-    pygame.display.flip() #cập nhật màn hình để hiển thị trình phát, tiểu hành tinh và điểm số.
+    pygame.display.flip() 
     #tick the clock
-    clock.tick(60) #kiểm soát tốc độ khung hình của trò chơi, với 60 khung hình mỗi giây.
+    clock.tick(60) 
